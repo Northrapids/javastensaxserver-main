15078093cc048833bba4de608a7e7718a94ad319
Northrapids <nordfors.fredrik@gmail.com>
2023-09-29 13:48:26 +0200

    removed @sprinTest

--- src/test/java/se/systementor/stengameserver/StengameserverApplicationTests.java
+++ src/test/java/se/systementor/stengameserver/StengameserverApplicationTests.java
@@ -3,11 +3,8 @@ package se.systementor.stengameserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class StengameserverApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
